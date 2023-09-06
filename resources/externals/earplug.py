997e6b2cbc01eaac6382d9cb055de7f7b7f1b649
Charles K. Neimog <charlesneimog@outlook.com>
2023-09-05 21:40:05 -0300

    minor updates

--- resources/externals/earplug.py
+++ resources/externals/earplug.py
@@ -2,13 +2,11 @@ import os
 import shutil
 
 def earplug_extra(librarySelf):
-    ''' 
+    '''
     This function copy some things that I already need to compile some externals in cyclone
     '''
-    # print in orange, executing earplug function   
-    print("\n")
+    # print in orange, executing earplug function
     print("\033[95m" + "    Executing earplug_extra function" + "\033[0m")
-    print("\n")
     if librarySelf.extraFuncExecuted:
         return
 
@@ -20,15 +18,13 @@ def earplug_extra(librarySelf):
 
     folder = librarySelf.folder
     shutil.copy(os.path.join(folder, "earplug_data.txt"), os.path.join(librarySelf.PROJECT_ROOT, "webpatch", "data/earplug_data.txt"))
-    
-            
 
 
 
-    
 
- 
-    
+
+
+
 
 
 
--- resources/externals/pdelse.py
+++ resources/externals/pdelse.py
@@ -1,1 +1,27 @@
+import os
+import shutil
 
+
+def else_extra(librarySelf):
+    '''
+    This function copy some things that I already need to compile some externals in cyclone
+    '''
+    if librarySelf.extraFuncExecuted:
+        return
+
+    if not os.path.exists(os.path.join(librarySelf.PROJECT_ROOT, "webpatch", "includes")):
+        os.makedirs(os.path.join(librarySelf.PROJECT_ROOT, "webpatch", "includes"))
+    folder = os.path.join(librarySelf.folder, "Code_source", "shared")
+
+    for file in os.listdir(folder):
+        if file.endswith(".h"):
+            shutil.copy(os.path.join(folder, file),
+                        os.path.join(librarySelf.PROJECT_ROOT, "webpatch", "includes"))
+
+    for file in os.listdir(folder):
+        if file.endswith(".c"):
+            shutil.copy(os.path.join(folder, file),
+                        os.path.join(librarySelf.PROJECT_ROOT, "webpatch", "externals"))
+
+    os.remove(os.path.join(librarySelf.PROJECT_ROOT, "webpatch", "externals", "s_elseutf8.c"))
+    librarySelf.extraFuncExecuted = True
