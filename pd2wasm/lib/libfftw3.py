37ba6a8f9ca0c2d241ab7ff1aa4a4408206e0955
Charles K. Neimog <charlesneimog@outlook.com>
2023-09-27 20:08:40 -0300

    minor fixes

--- pd2wasm/lib/libfftw3.py
+++ pd2wasm/lib/libfftw3.py
@@ -3,19 +3,15 @@ import tarfile
 import os
 from ..helpers import myprint, fixPaths
 from ..helpers import emccPaths
+import platform
 
 
 def downloadAndBuild_FFTW3(webpdPatchSelf): # defined in PdWebCompiler.py
     from ..pd2wasm import webpdPatch
     webpdPatchClass: webpdPatch = webpdPatchSelf # for better autocompletion
-
     PackagePatch = webpdPatchClass.PdWebCompilerPath
-
-        
-
+    
     if not os.path.exists(PackagePatch + "/.lib/fftw-3.3.10"):
-        # print in orange
-        print("\n")
         myprint("Downloading FFTW3...", color="orange")
         response = requests.get('https://www.fftw.org/fftw-3.3.10.tar.gz')
         outputFile = fixPaths(PackagePatch + '/.lib/fftw-3.3.10.tar.gz')
@@ -25,25 +21,21 @@ def downloadAndBuild_FFTW3(webpdPatchSelf): # defined in PdWebCompiler.py
             tar.extractall(PackagePatch + '/.lib')
         os.remove(PackagePatch + '/.lib/fftw-3.3.10.tar.gz')
 
-
-    # check if file projectRoot + "/.lib/fftw-3.3.10/.libs/libfftw3f.a" exists
     if os.path.exists(fixPaths(PackagePatch + "/.lib/fftw-3.3.10/.libs/libfftw3f.a")):
         webpdPatchClass.extraFlags.append(fixPaths("-I" + PackagePatch + "/.lib/fftw-3.3.10/api"))
         webpdPatchClass.extraFlags.append(fixPaths("-L" + PackagePatch + "/.lib/fftw-3.3.10/.libs"))
         webpdPatchClass.extraFlags.append("-lfftw3f")
         return True
 
-    # go to the fftw folder
-    print("\n")
-    print("\033[33m" + "    Building fftw3..." + "\033[0m")
-    print("\n")
-    
+    myprint("Building fftw3...", color="orange")
     compilers = emccPaths()
-
-
-    command = fixPaths("cd '" + PackagePatch + "/.lib/fftw-3.3.10'")
+    command = ("cmd /C cd '" + fixPaths(PackagePatch + "/.lib/fftw-3.3.10'"))
     command += f" && {compilers.configure} ./configure --enable-float --disable-fortran"
     command += f" && {compilers.make}"
+    
+    if platform.system() == "Windows":
+        command = command.replace("&&", "&")
+    myprint(command, color="red")
     os.system(command)
 
     webpdPatchClass.extraFlags.append(fixPaths("-I" + PackagePatch + "/.lib/fftw-3.3.10/api"))
