2c81a3faa6c208ef3744b9fed6e275c887497cbf
Charles K. Neimog <charlesneimog@outlook.com>
2023-10-05 12:14:51 -0300

    fix bad 1.1.2 release

--- pd4web/src/enable-threads.js
+++ pd4web/src/enable-threads.js
@@ -52,16 +52,16 @@ if(typeof window === 'undefined') {
 
     let registration = await navigator.serviceWorker.register(window.document.currentScript.src).catch(e => console.error("COOP/COEP Service Worker failed to register:", e));
     if(registration) {
-      console.log("COOP/COEP Service Worker registered", registration.scope);
+      console.error("COOP/COEP Service Worker registered", registration.scope);
 
       registration.addEventListener("updatefound", () => {
-        console.log("Reloading page to make use of updated COOP/COEP Service Worker.");
+        console.error("Reloading page to make use of updated COOP/COEP Service Worker.");
         window.location.reload();
       });
 
       // If the registration is active, but it's not controlling the page
       if(registration.active && !navigator.serviceWorker.controller) {
-        console.log("Reloading page to make use of COOP/COEP Service Worker.");
+        console.error("Reloading page to make use of COOP/COEP Service Worker.");
         window.location.reload();
       }
     }
