392656bd08300f06c90adfe49e3c0e64b3ca51a0
Charles K. Neimog <charlesneimog@outlook.com>
2023-10-18 10:25:19 -0300

    format template code

--- pd4web/src/template.c
+++ pd4web/src/template.c
@@ -36,20 +36,20 @@ char *HTML_IDS[] = {};
 int HTML_IDS_SIZE = 0;
 
 typedef struct pdItem {
-    const char *receiverID;
-    float f;
-    const char *s;
-    t_atom *list;
-    int listSize;
-    int type;
-    int changed;
+  const char *receiverID;
+  float f;
+  const char *s;
+  t_atom *list;
+  int listSize;
+  int type;
+  int changed;
 } pdItem;
 
 // ====================
 typedef struct pdItemHash {
-    pdItem **items;
-    int size;
-    int count;
+  pdItem **items;
+  int size;
+  int count;
 } pdItemHash;
 
 struct pdItemHash *receiverHash;
@@ -59,120 +59,120 @@ int pdWebValueArraySize = 32;
 
 // ====================
 static pdItemHash *CreatePdItemHash(int size) {
-    pdItemHash *hash_table = (pdItemHash *)malloc(sizeof(pdItemHash));
-    hash_table->size = size;
-    hash_table->count = 0;
-    hash_table->items = (pdItem **)calloc(size, sizeof(pdItem *));
-    return hash_table;
+  pdItemHash *hash_table = (pdItemHash *)malloc(sizeof(pdItemHash));
+  hash_table->size = size;
+  hash_table->count = 0;
+  hash_table->items = (pdItem **)calloc(size, sizeof(pdItem *));
+  return hash_table;
 }
 
 // ====================
 static unsigned int HashFunction(pdItemHash *hash_table, const char *key) {
-    unsigned long hash = 5381;
-    int c;
-    while ((c = *key++)) {
-        hash = ((hash << 5) + hash) + c;
-    }
-    return hash % hash_table->size;
+  unsigned long hash = 5381;
+  int c;
+  while ((c = *key++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
+  return hash % hash_table->size;
 }
 
 // ====================
 static void InsertList(pdItemHash *hash_table, const char *key, int size,
                        t_atom *list) {
-    unsigned int index = HashFunction(hash_table, key);
-    pthread_mutex_lock(&WriteReadMutex);
-    pdItem *item = hash_table->items[index];
-    if (item == NULL && hash_table->count <= hash_table->size) {
-        item = (pdItem *)malloc(sizeof(pdItem));
-        item->receiverID = strdup(key);
-        item->list = malloc(sizeof(t_atom) * size);
-        memcpy(item->list, list, sizeof(t_atom) * size);
-        item->listSize = size;
-        item->type = A_GIMME;
-        item->changed = 1;
-        hash_table->items[index] = item;
-        hash_table->count++;
-    } else if (hash_table->count > hash_table->size) {
-        EM_ASM_({
-            alert("Hash table is full, please report this in " +
-                  "github.com/charlesneimog/pd4web");
-        });
-    } else if (item != NULL) {
-        item->list = list;
-        item->changed = 1;
-        item->type = A_GIMME;
-    }
-    pthread_mutex_unlock(&WriteReadMutex);
-    return;
+  unsigned int index = HashFunction(hash_table, key);
+  pthread_mutex_lock(&WriteReadMutex);
+  pdItem *item = hash_table->items[index];
+  if (item == NULL && hash_table->count <= hash_table->size) {
+    item = (pdItem *)malloc(sizeof(pdItem));
+    item->receiverID = strdup(key);
+    item->list = malloc(sizeof(t_atom) * size);
+    memcpy(item->list, list, sizeof(t_atom) * size);
+    item->listSize = size;
+    item->type = A_GIMME;
+    item->changed = 1;
+    hash_table->items[index] = item;
+    hash_table->count++;
+  } else if (hash_table->count > hash_table->size) {
+    EM_ASM_({
+      alert("Hash table is full, please report this in " +
+            "github.com/charlesneimog/pd4web");
+    });
+  } else if (item != NULL) {
+    item->list = list;
+    item->changed = 1;
+    item->type = A_GIMME;
+  }
+  pthread_mutex_unlock(&WriteReadMutex);
+  return;
 }
 
 // ====================
 static void InsertFloat(pdItemHash *hash_table, const char *key, float f) {
-    unsigned int index = HashFunction(hash_table, key);
-    pthread_mutex_lock(&WriteReadMutex);
-    pdItem *item = hash_table->items[index];
-    if (item == NULL && hash_table->count <= hash_table->size) {
-        item = (pdItem *)malloc(sizeof(pdItem));
-        item->receiverID = strdup(key);
-        item->f = f;
-        item->type = A_FLOAT;
-        item->changed = 1;
-        hash_table->items[index] = item;
-        hash_table->count++;
-    } else if (hash_table->count > hash_table->size) {
-        EM_ASM_({
-            alert("Hash table is full, please report this in " +
-                  "github.com/charlesneimog/pd4web");
-        });
-    } else if (item != NULL) {
-        item->f = f;
-        item->changed = 1;
-        item->type = A_FLOAT;
-    }
-    pthread_mutex_unlock(&WriteReadMutex);
-    return;
+  unsigned int index = HashFunction(hash_table, key);
+  pthread_mutex_lock(&WriteReadMutex);
+  pdItem *item = hash_table->items[index];
+  if (item == NULL && hash_table->count <= hash_table->size) {
+    item = (pdItem *)malloc(sizeof(pdItem));
+    item->receiverID = strdup(key);
+    item->f = f;
+    item->type = A_FLOAT;
+    item->changed = 1;
+    hash_table->items[index] = item;
+    hash_table->count++;
+  } else if (hash_table->count > hash_table->size) {
+    EM_ASM_({
+      alert("Hash table is full, please report this in " +
+            "github.com/charlesneimog/pd4web");
+    });
+  } else if (item != NULL) {
+    item->f = f;
+    item->changed = 1;
+    item->type = A_FLOAT;
+  }
+  pthread_mutex_unlock(&WriteReadMutex);
+  return;
 }
 
 // ====================
 static void InsertSymbol(pdItemHash *hash_table, const char *key,
                          const char *thing) {
-    unsigned int index = HashFunction(hash_table, key);
-    pthread_mutex_lock(&WriteReadMutex);
-    pdItem *item = hash_table->items[index];
-    if (item == NULL && hash_table->count <= hash_table->size) {
-        item = (pdItem *)malloc(sizeof(pdItem));
-        item->receiverID = strdup(key);
-        item->s = strdup(thing);
-        item->type = A_SYMBOL;
-        item->changed = 1;
-        hash_table->items[index] = item;
-        hash_table->count++;
-    } else if (hash_table->count > hash_table->size) {
-        EM_ASM_({
-            alert("Hash table is full, please report this in " +
-                  "github.com/charlesneimog/pd4web");
-        });
-    } else if (item != NULL) {
-        item->s = strdup(thing);
-        item->changed = 1;
-        item->type = A_SYMBOL;
-    }
-    pthread_mutex_unlock(&WriteReadMutex);
-    return;
+  unsigned int index = HashFunction(hash_table, key);
+  pthread_mutex_lock(&WriteReadMutex);
+  pdItem *item = hash_table->items[index];
+  if (item == NULL && hash_table->count <= hash_table->size) {
+    item = (pdItem *)malloc(sizeof(pdItem));
+    item->receiverID = strdup(key);
+    item->s = strdup(thing);
+    item->type = A_SYMBOL;
+    item->changed = 1;
+    hash_table->items[index] = item;
+    hash_table->count++;
+  } else if (hash_table->count > hash_table->size) {
+    EM_ASM_({
+      alert("Hash table is full, please report this in " +
+            "github.com/charlesneimog/pd4web");
+    });
+  } else if (item != NULL) {
+    item->s = strdup(thing);
+    item->changed = 1;
+    item->type = A_SYMBOL;
+  }
+  pthread_mutex_unlock(&WriteReadMutex);
+  return;
 }
 
 // ====================
 static pdItem *GetItem(pdItemHash *hash_table, char *key) {
-    unsigned int index = HashFunction(hash_table, key);
-    pthread_mutex_lock(&WriteReadMutex);
+  unsigned int index = HashFunction(hash_table, key);
+  pthread_mutex_lock(&WriteReadMutex);
 
-    pdItem *item = hash_table->items[index];
-    if (item != NULL) {
-        pthread_mutex_unlock(&WriteReadMutex);
-        return item;
-    }
+  pdItem *item = hash_table->items[index];
+  if (item != NULL) {
     pthread_mutex_unlock(&WriteReadMutex);
-    return NULL;
+    return item;
+  }
+  pthread_mutex_unlock(&WriteReadMutex);
+  return NULL;
 }
 
 // =====================================
@@ -190,13 +190,13 @@ void webpd_free(void *ptr) { free(ptr); }
 // ======================================
 EMSCRIPTEN_KEEPALIVE
 int sendFloatToPd(const char *receiver, float value) {
-    return libpd_float(receiver, value);
+  return libpd_float(receiver, value);
 }
 
 // ======================================
 EMSCRIPTEN_KEEPALIVE
 int sendSymbolToPd(const char *receiver, const char *symbol) {
-    return libpd_symbol(receiver, symbol);
+  return libpd_symbol(receiver, symbol);
 }
 
 // ======================================
@@ -208,25 +208,25 @@ int sendBangToPd(const char *receiver) { return libpd_bang(receiver); }
 // ======================================
 
 void pdprint(const char *s) {
-    if (s[0] == '\n') {
-        return;
-    }
-    EM_ASM_({ console.log(UTF8ToString($0)); }, s);
+  if (s[0] == '\n') {
+    return;
+  }
+  EM_ASM_({ console.log(UTF8ToString($0)); }, s);
 }
 
 // ========================================
 void receiveListfromPd(const char *src, int argc, t_atom *argv) {
-    InsertList(receiverHash, (char *)src, argc, argv);
+  InsertList(receiverHash, (char *)src, argc, argv);
 }
 
 // ========================================
 void receiveFloatfromPd(const char *receiver, float value) {
-    InsertFloat(receiverHash, (char *)receiver, value);
+  InsertFloat(receiverHash, (char *)receiver, value);
 }
 
 // ========================================
 static void receiveSymbolfromPd(const char *receiver, const char *thing) {
-    InsertSymbol(receiverHash, (char *)receiver, (char *)thing);
+  InsertSymbol(receiverHash, (char *)receiver, (char *)thing);
 }
 
 // ========================================
@@ -238,20 +238,19 @@ static EM_BOOL ProcessPdPatch(int numInputs, const AudioSampleFrame *inputs,
                               int numOutputs, AudioSampleFrame *outputs,
                               int numParams, const AudioParamFrame *params,
                               void *userData) {
-    int outCh = outputs[0].numberOfChannels;
-    float tmpOutputs[128 * 2];
-    libpd_process_float(
-        2, inputs[0].data,
-        tmpOutputs); // TODO: ADD INPUTS AND OUTPUTS FOR pd2wasm.
-    int outputIndex = 0;
-    for (int i = 0; i < outCh; i++) {
-        for (int j = i; j < (128 * outCh); j += 2) {
-            outputs[0].data[outputIndex] = tmpOutputs[j];
-            outputIndex++;
-        }
+  int outCh = outputs[0].numberOfChannels;
+  float tmpOutputs[128 * 2];
+  libpd_process_float(2, inputs[0].data,
+                      tmpOutputs); // TODO: ADD INPUTS AND OUTPUTS FOR pd2wasm.
+  int outputIndex = 0;
+  for (int i = 0; i < outCh; i++) {
+    for (int j = i; j < (128 * outCh); j += 2) {
+      outputs[0].data[outputIndex] = tmpOutputs[j];
+      outputIndex++;
     }
-    libpd_bang("pd4webtick");
-    return EM_TRUE;
+  }
+  libpd_bang("pd4webtick");
+  return EM_TRUE;
 }
 // ========================================
 // clang-format off
@@ -274,60 +273,60 @@ EM_JS(void, AddUIButtons, (EMSCRIPTEN_WEBAUDIO_T audioContext, EMSCRIPTEN_AUDIO_
 // ========================================
 void AudioWorkletProcessorCreated(EMSCRIPTEN_WEBAUDIO_T audioContext,
                                   EM_BOOL success, void *userData) {
-    if (!success) {
-        return;
-    }
-    int outputChannelCounts[1] = {2};
-    /* Stereo output TODO: Need to see how this
-      work for more than 2 channels */
-    EmscriptenAudioWorkletNodeCreateOptions options = {
-        .numberOfInputs = 1,
-        .numberOfOutputs = 1,
-        .outputChannelCounts = outputChannelCounts,
-    };
+  if (!success) {
+    return;
+  }
+  int outputChannelCounts[1] = {2};
+  /* Stereo output TODO: Need to see how this
+    work for more than 2 channels */
+  EmscriptenAudioWorkletNodeCreateOptions options = {
+      .numberOfInputs = 1,
+      .numberOfOutputs = 1,
+      .outputChannelCounts = outputChannelCounts,
+  };
 
-    EMSCRIPTEN_AUDIO_WORKLET_NODE_T wasmAudioWorklet =
-        emscripten_create_wasm_audio_worklet_node(
-            audioContext, "libpd-processor", &options, &ProcessPdPatch, 0);
-    AddUIButtons(audioContext, wasmAudioWorklet);
-    libpd_set_listhook(receiveListfromPd);
-    libpd_set_floathook(receiveFloatfromPd);
-    libpd_set_symbolhook(receiveSymbolfromPd);
-    libpd_set_printhook(pdprint);
-    libpd_init();
+  EMSCRIPTEN_AUDIO_WORKLET_NODE_T wasmAudioWorklet =
+      emscripten_create_wasm_audio_worklet_node(audioContext, "libpd-processor",
+                                                &options, &ProcessPdPatch, 0);
+  AddUIButtons(audioContext, wasmAudioWorklet);
+  libpd_set_listhook(receiveListfromPd);
+  libpd_set_floathook(receiveFloatfromPd);
+  libpd_set_symbolhook(receiveSymbolfromPd);
+  libpd_set_printhook(pdprint);
+  libpd_init();
 
-    // WebPd Load Externals
+  // WebPd Load Externals
 
-    // ====================
+  // ====================
 
-    for (int i = 0; i < HTML_IDS_SIZE; i++) {
-        libpd_bind(HTML_IDS[i]);
-    }
-    libpd_add_to_search_path("webpatch/data/");
-    libpd_add_to_search_path("webpatch/data/Audios/");
-    libpd_start_message(1);
-    libpd_add_float(1.0f);
-    libpd_finish_message("pd", "dsp");
-    libpd_init_audio(patchAudioInputs, patchAudioOutputs,
-                     GetAudioSampleRate(audioContext));
-    if (!libpd_openfile("index.pd", "webpatch/data")) {
-        EM_ASM_({ alert("Failed to open patch!"); });
-        return;
-    }
-    EM_ASM_({ JS_LoadFinished(); });
+  for (int i = 0; i < HTML_IDS_SIZE; i++) {
+    libpd_bind(HTML_IDS[i]);
+  }
+  libpd_add_to_search_path("webpatch/data/");
+  libpd_add_to_search_path("webpatch/data/Audios/");
+  libpd_start_message(1);
+  libpd_add_float(1.0f);
+  libpd_finish_message("pd", "dsp");
+  libpd_init_audio(patchAudioInputs, patchAudioOutputs,
+                   GetAudioSampleRate(audioContext));
+  if (!libpd_openfile("index.pd", "webpatch/data")) {
+    EM_ASM_({ alert("Failed to open patch!"); });
+    return;
+  }
+  EM_ASM_({ JS_LoadFinished(); });
 }
 
 // ========================================
 void WebAudioWorkletThreadInitialized(EMSCRIPTEN_WEBAUDIO_T audioContext,
                                       EM_BOOL success, void *userData) {
-    if (!success) {
-        return;
-    }
-    WebAudioWorkletProcessorCreateOptions opts = {
-        .name = "libpd-processor",
-    };
-    emscripten_create_wasm_audio_worklet_processor_async(
-        audioContext, &opts, AudioWorkletProcessorCreated, 0);
+  if (!success) {
+    return;
+  }
+  WebAudioWorkletProcessorCreateOptions opts = {
+      .name = "libpd-processor",
+  };
+  emscripten_create_wasm_audio_worklet_processor_async(
+      audioContext, &opts, AudioWorkletProcessorCreated, 0);
 }
 
 // ========================================
@@ -335,7 +334,6 @@ void WebAudioWorkletThreadInitialized(EMSCRIPTEN_WEBAUDIO_T audioContext,
 EM_JS(void, setFloatValue, (const char* symbol, float value), {
     var symbolId = UTF8ToString(symbol);
     var element = document.getElementById(symbolId); // Find the element by ID
-    console.log(element);
     if (element === null) {
         var myElement = document.createElement("input");
         myElement.id = symbolId;
