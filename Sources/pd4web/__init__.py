a32df00aff5b7291ea2e62f63a98786f6994e8e1
Charles K. Neimog <charlesneimog@outlook.com>
2024-06-15 20:34:04 -0300

    remove old pd4web

--- Sources/pd4web/__init__.py
+++ Sources/pd4web/__init__.py
@@ -1,1 +1,5 @@
-from .pd4web import webpdPatch
+from .GetCode import GetCode
+from .Helpers import getPrintValue, pd4web_print
+from .Libraries import ExternalLibraries
+from .Objects import PdObjects
+from .Patch import Patch, PatchLine
