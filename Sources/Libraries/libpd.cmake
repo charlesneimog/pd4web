3e444422789c6a2b8abab64c082eb9eb97fd5e2e
Charles K. Neimog <charlesneimog@outlook.com>
2024-07-22 15:12:45 -0300

    set puredata source for libpd.cmake

--- Sources/Libraries/libpd.cmake
+++ Sources/Libraries/libpd.cmake
@@ -1,122 +1,124 @@
 cmake_minimum_required(VERSION 3.25)
 project(libpd C)
 
+set(PD_SOURCE_DIR
+    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/
+    CACHE STRING "PATH where is located PureData Source Code")
+
+message(STATUS "PD_SOURCES: ${PD_SOURCE_DIR}")
+
+# ╭──────────────────────────────────────╮
+# │           PureData Sources           │
+# ╰──────────────────────────────────────╯
 set(PD_SOURCES
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_arithmetic.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_array.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_ctl.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_dac.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_delay.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_fft.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_fft_fftsg.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_filter.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_global.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_math.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_misc.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_osc.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_resample.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_soundfile.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_soundfile_aiff.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_soundfile_caf.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_soundfile_next.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_soundfile_wave.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/d_ugen.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_all_guis.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_array.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_bang.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_canvas.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_clone.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_editor.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_editor_extras.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_graph.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_guiconnect.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_io.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_mycanvas.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_numbox.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_radio.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_readwrite.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_rtext.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_scalar.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_slider.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_template.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_text.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_toggle.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_traversal.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_undo.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/g_vumeter.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_atom.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_binbuf.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_class.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_conf.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_glob.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_imp.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_memory.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_obj.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_pd.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/m_sched.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_audio.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_audio_dummy.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_inter.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_inter_gui.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_loader.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_main.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_net.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_path.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_print.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_utf8.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_acoustics.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_arithmetic.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_array.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_connective.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_file.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_gui.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_interface.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_list.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_midi.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_misc.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_net.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_scalar.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_text.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_time.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_vexp.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_vexp_fun.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_vexp_if.c)
+    ${PD_SOURCE_DIR}/d_arithmetic.c
+    ${PD_SOURCE_DIR}/d_array.c
+    ${PD_SOURCE_DIR}/d_ctl.c
+    ${PD_SOURCE_DIR}/d_dac.c
+    ${PD_SOURCE_DIR}/d_delay.c
+    ${PD_SOURCE_DIR}/d_fft.c
+    ${PD_SOURCE_DIR}/d_fft_fftsg.c
+    ${PD_SOURCE_DIR}/d_filter.c
+    ${PD_SOURCE_DIR}/d_global.c
+    ${PD_SOURCE_DIR}/d_math.c
+    ${PD_SOURCE_DIR}/d_misc.c
+    ${PD_SOURCE_DIR}/d_osc.c
+    ${PD_SOURCE_DIR}/d_resample.c
+    ${PD_SOURCE_DIR}/d_soundfile.c
+    ${PD_SOURCE_DIR}/d_soundfile_aiff.c
+    ${PD_SOURCE_DIR}/d_soundfile_caf.c
+    ${PD_SOURCE_DIR}/d_soundfile_next.c
+    ${PD_SOURCE_DIR}/d_soundfile_wave.c
+    ${PD_SOURCE_DIR}/d_ugen.c
+    ${PD_SOURCE_DIR}/g_all_guis.c
+    ${PD_SOURCE_DIR}/g_array.c
+    ${PD_SOURCE_DIR}/g_bang.c
+    ${PD_SOURCE_DIR}/g_canvas.c
+    ${PD_SOURCE_DIR}/g_clone.c
+    ${PD_SOURCE_DIR}/g_editor.c
+    ${PD_SOURCE_DIR}/g_editor_extras.c
+    ${PD_SOURCE_DIR}/g_graph.c
+    ${PD_SOURCE_DIR}/g_guiconnect.c
+    ${PD_SOURCE_DIR}/g_io.c
+    ${PD_SOURCE_DIR}/g_mycanvas.c
+    ${PD_SOURCE_DIR}/g_numbox.c
+    ${PD_SOURCE_DIR}/g_radio.c
+    ${PD_SOURCE_DIR}/g_readwrite.c
+    ${PD_SOURCE_DIR}/g_rtext.c
+    ${PD_SOURCE_DIR}/g_scalar.c
+    ${PD_SOURCE_DIR}/g_slider.c
+    ${PD_SOURCE_DIR}/g_template.c
+    ${PD_SOURCE_DIR}/g_text.c
+    ${PD_SOURCE_DIR}/g_toggle.c
+    ${PD_SOURCE_DIR}/g_traversal.c
+    ${PD_SOURCE_DIR}/g_undo.c
+    ${PD_SOURCE_DIR}/g_vumeter.c
+    ${PD_SOURCE_DIR}/m_atom.c
+    ${PD_SOURCE_DIR}/m_binbuf.c
+    ${PD_SOURCE_DIR}/m_class.c
+    ${PD_SOURCE_DIR}/m_conf.c
+    ${PD_SOURCE_DIR}/m_glob.c
+    ${PD_SOURCE_DIR}/m_imp.h
+    ${PD_SOURCE_DIR}/m_memory.c
+    ${PD_SOURCE_DIR}/m_obj.c
+    ${PD_SOURCE_DIR}/m_pd.c
+    ${PD_SOURCE_DIR}/m_sched.c
+    ${PD_SOURCE_DIR}/s_audio.c
+    ${PD_SOURCE_DIR}/s_audio_dummy.c
+    ${PD_SOURCE_DIR}/s_inter.c
+    ${PD_SOURCE_DIR}/s_inter_gui.c
+    ${PD_SOURCE_DIR}/s_loader.c
+    ${PD_SOURCE_DIR}/s_main.c
+    ${PD_SOURCE_DIR}/s_net.c
+    ${PD_SOURCE_DIR}/s_path.c
+    ${PD_SOURCE_DIR}/s_print.c
+    ${PD_SOURCE_DIR}/s_utf8.c
+    ${PD_SOURCE_DIR}/x_acoustics.c
+    ${PD_SOURCE_DIR}/x_arithmetic.c
+    ${PD_SOURCE_DIR}/x_array.c
+    ${PD_SOURCE_DIR}/x_connective.c
+    ${PD_SOURCE_DIR}/x_file.c
+    ${PD_SOURCE_DIR}/x_gui.c
+    ${PD_SOURCE_DIR}/x_interface.c
+    ${PD_SOURCE_DIR}/x_list.c
+    ${PD_SOURCE_DIR}/x_midi.c
+    ${PD_SOURCE_DIR}/x_misc.c
+    ${PD_SOURCE_DIR}/x_net.c
+    ${PD_SOURCE_DIR}/x_scalar.c
+    ${PD_SOURCE_DIR}/x_text.c
+    ${PD_SOURCE_DIR}/x_time.c
+    ${PD_SOURCE_DIR}/x_vexp.c
+    ${PD_SOURCE_DIR}/x_vexp_fun.c
+    ${PD_SOURCE_DIR}/x_vexp_if.c)
 
 # TODO: Add the following sources
-set(PD_BOB_TILDE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/bob~/bob~.c)
-set(PD_BONK_TILDE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/bonk~/bonk~.c)
-set(PD_CHOICE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/choice/choice.c)
-set(PD_FIDDLE_TILDE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/fiddle~/fiddle~.c)
-set(PD_LOOP_TILDE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/loop~/loop~.c)
-set(PD_LRSHIFT_TILDE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/lrshift~/lrshift~.c)
-set(PD_PDSCHED_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/pd~/pdsched.c)
-# set(PD_PD_TILDE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/pd~/pd~.c)
-set(PD_PIQUE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/pique/pique.c)
-set(PD_SIGMUND_TILDE_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/sigmund~/sigmund~.c)
-set(PD_STDOUT_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/extra/stdout/stdout.c)
+set(PD_BOB_TILDE_SOURCE ${PD_SOURCE_DIR}/../extra/bob~/bob~.c)
+set(PD_BONK_TILDE_SOURCE ${PD_SOURCE_DIR}/../extra/bonk~/bonk~.c)
+set(PD_CHOICE_SOURCE ${PD_SOURCE_DIR}/../extra/choice/choice.c)
+set(PD_FIDDLE_TILDE_SOURCE ${PD_SOURCE_DIR}/../extra/fiddle~/fiddle~.c)
+set(PD_LOOP_TILDE_SOURCE ${PD_SOURCE_DIR}/../extra/loop~/loop~.c)
+set(PD_LRSHIFT_TILDE_SOURCE ${PD_SOURCE_DIR}/../extra/lrshift~/lrshift~.c)
+set(PD_PDSCHED_SOURCE ${PD_SOURCE_DIR}/../extra/pd~/pdsched.c)
+# set(PD_PD_TILDE_SOURCE ${PD_SOURCE_DIR}/../extra/pd~/pd~.c)
+set(PD_PIQUE_SOURCE ${PD_SOURCE_DIR}/../extra/pique/pique.c)
+set(PD_SIGMUND_TILDE_SOURCE ${PD_SOURCE_DIR}/../extra/sigmund~/sigmund~.c)
+set(PD_STDOUT_SOURCE ${PD_SOURCE_DIR}/../extra/stdout/stdout.c)
 
-set(LIBPD_MAIN_SOURCES
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/s_libpdmidi.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_libpdreceive.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/x_libpdreceive.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/z_hooks.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/z_hooks.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/z_libpd.c)
-
-set(LIBPD_UTILS_SOURCES
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/util/z_ringbuffer.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/util/z_ringbuffer.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/util/z_print_util.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/util/z_print_util.h
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/util/z_queued.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src/util/z_queued.h)
-
-set(LIBPD_SOURCES ${PD_SOURCES} ${LIBPD_MAIN_SOURCES})
+# ╭──────────────────────────────────────╮
+# │            Libpd Sources             │
+# ╰──────────────────────────────────────╯
+set(LIBPD_MAIN_SOURCES ${PD_SOURCE_DIR}/x_libpdreceive.c ${PD_SOURCE_DIR}/s_libpdmidi.c
+                       ${PD_SOURCE_DIR}/z_hooks.c ${PD_SOURCE_DIR}/z_libpd.c)
+set(LIBPD_UTILS_SOURCES ${PD_SOURCE_DIR}/z_ringbuffer.c ${PD_SOURCE_DIR}/z_print_util.c
+                        ${PD_SOURCE_DIR}/z_queued.c)
+set(LIBPD_SOURCES ${PD_SOURCES} ${LIBPD_MAIN_SOURCES} ${LIBPD_UTILS_SOURCES})
 
 add_library(libpd STATIC ${LIBPD_SOURCES})
 set_target_properties(libpd PROPERTIES OUTPUT_NAME pd)
-target_compile_definitions(libpd PRIVATE -DPD=1 -DUSEAPI_DUMMY=1 -DHAVE_UNISTD_H=1 -DHAVE_LIBDL -DPD_INTERNAL)
+target_compile_definitions(libpd PRIVATE -DPD=1 -DUSEAPI_DUMMY=1 -DHAVE_UNISTD_H=1 -DHAVE_LIBDL
+                                         -DPD_INTERNAL)
 target_compile_options(libpd PRIVATE -w)
-target_compile_options(libpd PRIVATE $<$<CONFIG:Release>:-ffast-math> $<$<CONFIG:Release>:-funroll-loops>
-                                     $<$<CONFIG:Release>:-fomit-frame-pointer> $<$<CONFIG:Release>:-O3>)
-target_include_directories(libpd PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/Pd4Web/pure-data/src)
+target_compile_options(
+    libpd PRIVATE $<$<CONFIG:Release>:-ffast-math> $<$<CONFIG:Release>:-funroll-loops>
+                  $<$<CONFIG:Release>:-fomit-frame-pointer> $<$<CONFIG:Release>:-O3>)
+target_include_directories(libpd PRIVATE ${PD_SOURCE_DIR}/src)
