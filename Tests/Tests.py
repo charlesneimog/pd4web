28be7a41c19a06669eb5a96662c4ab827373623e
Charles K. Neimog <charlesneimog@outlook.com>
2024-07-30 19:22:51 -0300

    fix test

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -38,17 +38,15 @@ def execute_chrome(path, port):
 
     @app.route("/testar")
     def testar(localport):
-        driver = webdriver.Chrome(
-            service=chrome_service, options=chrome_options)
+        driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
         driver.get(f"http://localhost:{localport}")
         time.sleep(1)  # Wait
 
         try:
-            element = driver.find_element(
-                By.XPATH, '//*[@id="Pd4WebAudioSwitch"]')
+            element = driver.find_element(By.XPATH, '//*[@id="Pd4WebAudioSwitch"]')
 
             element.click()
-            time.sleep(5)  # run for 5 seconds
+            time.sleep(2)  # run for 5 seconds
             logs = driver.get_log("browser")
 
             for log_entry in logs:
@@ -62,8 +60,7 @@ def execute_chrome(path, port):
         return "Teste concluído!"
 
     # Inicie o servidor Flask em um processo separado
-    server_process = multiprocessing.Process(
-        target=app.run, kwargs={"debug": False, "use_reloader": False})
+    server_process = multiprocessing.Process(target=app.run, kwargs={"debug": False, "use_reloader": False})
     server_process.start()
 
     try:
@@ -74,60 +71,25 @@ def execute_chrome(path, port):
         time.sleep(1)  # Ensure the port is released before the next test
 
 
-class TestMyModule(unittest.TestCase):
-    @classmethod
-    def tearDownClass(cls):
-        for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
-            for file in files:
-                if file == "CMakeLists.txt":
-                    os.remove(os.path.join(root, file))
-                if file == "favicon.ico":
-                    os.remove(os.path.join(root, file))
-                if file == "index.html":
-                    os.remove(os.path.join(root, file))
-            for dir in dirs:
-                if dir == ".git":
-                    shutil.rmtree(os.path.join(root, dir))
-                if dir == "WebPatch":
-                    shutil.rmtree(os.path.join(root, dir))
-                if dir == "Pd4Web":
-                    shutil.rmtree(os.path.join(root, dir))
-                if dir == "build":
-                    shutil.rmtree(os.path.join(root, dir))
+def RunTest(patchPath, port):
+    string = f"Testing {patchPath}"
+    print(f"\n\n\n{'='*len(string)}\n{string}\n{'='*len(string)}")
+    this_file = os.path.abspath(__file__)
+    pd_file = os.path.join(os.path.dirname(this_file), patchPath)
+    Pd4WebInstance = Pd4Web.Pd4Web(Patch=pd_file)
+    # Pd4WebInstance.SILENCE = True
+    Pd4WebInstance.verbose = True
+    Pd4WebInstance.Execute()
 
-    def RunTest(self, patchPath, port):
-        string = f"Testing {patchPath}"
-        print(f"\n\n\n{'='*len(string)}\n{string}\n{'='*len(string)}")
+    patchDir = os.path.dirname(pd_file)
+    execute_chrome(patchDir, port)
+    print("\n\n")
 
-        this_file = os.path.abspath(__file__)
-        pd_file = os.path.join(os.path.dirname(this_file), patchPath)
-        Pd4WebInstance = Pd4Web.Pd4Web(Patch=pd_file)
-        # Pd4WebInstance.SILENCE = True
-        Pd4WebInstance.verbose = True
-        Pd4WebInstance.Execute()
 
-        patchDir = os.path.dirname(pd_file)
-        execute_chrome(patchDir, port)
-        print("\n\n")
-
-    def test_ObjectNotFound(self):
-        with self.assertRaises(ValueError) as context:
-            self.RunTest("Basic/objnotfound.pd", 5000)
-        if "Object not found:" not in str(context.exception):
-            raise Exception("Object not found not raised")
-
-    def test_pmpd(self):
-        self.RunTest("Libraries/pmpd-noaudio.pd", 5000)
-
-        with self.assertRaises(ValueError) as context:
-            self.RunTest("Libraries/pmpd-audio.pd", 5000)
-        if "not supported by Pd4Web yet." not in str(context.exception):
-            raise Exception("pmpd not raised")
-
+class Pd4webElse(unittest.TestCase):
     def test_else(self):
         lib = os.path.join(os.path.dirname(__file__), "Libraries/else")
-        all_files = os.listdir(os.path.join(
-            os.path.dirname(__file__), "Libraries/else"))
+        all_files = os.listdir(os.path.join(os.path.dirname(__file__), "Libraries/else"))
         pd_files = sorted([obj for obj in all_files if obj.endswith(".pd")])
 
         for i in range(len(pd_files)):
@@ -136,18 +98,39 @@ class TestMyModule(unittest.TestCase):
             patchname = filename.split(".")[0]
             os.makedirs(f"{lib}/else/", exist_ok=True)
             os.makedirs(f"{lib}/else/{patchname}", exist_ok=True)
-            shutil.copyfile(f"{lib}/{file_path}",
-                            f"{lib}/else/{patchname}/{filename}")
+            shutil.copyfile(f"{lib}/{file_path}", f"{lib}/else/{patchname}/{filename}")
             newpatch = f"{lib}/else/{patchname}/{filename}"
             try:
-                self.RunTest(newpatch, 5000)
+                RunTest(newpatch, 5000)
             except:
-                pass
-
-            self.RunTest(newpatch, 5000)
-
+                ## print in red
+                print(f"\033[91mError: {newpatch} -- trying again\033[0m")
+                RunTest(newpatch, 5000)
         shutil.rmtree(f"{lib}/else")
 
 
+class Pd4webPmpd(unittest.TestCase):
+    def test_pmpd(self):
+        lib = os.path.join(os.path.dirname(__file__), "Libraries/pmpd")
+        all_files = os.listdir(os.path.join(os.path.dirname(__file__), "Libraries/pmpd"))
+        pd_files = sorted([obj for obj in all_files if obj.endswith(".pd")])
+
+        for i in range(len(pd_files)):
+            file_path = pd_files[i]
+            filename = file_path.split("/")[-1]
+            patchname = filename.split(".")[0]
+            os.makedirs(f"{lib}/pmpd/", exist_ok=True)
+            os.makedirs(f"{lib}/pmpd/{patchname}", exist_ok=True)
+            shutil.copyfile(f"{lib}/{file_path}", f"{lib}/pmpd/{patchname}/{filename}")
+            newpatch = f"{lib}/pmpd/{patchname}/{filename}"
+            try:
+                RunTest(newpatch, 5000)
+            except:
+                print(f"\033[91mError: {newpatch} -- trying again\033[0m")
+                RunTest(newpatch, 5000)
+                pass
+        shutil.rmtree(f"{lib}/pmpd")
+
+
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(failfast=True)
