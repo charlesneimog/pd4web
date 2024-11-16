7b3ec8c2bacb2ece616113ed127f9b969830a29d
Charles K. Neimog <charlesneimog@outlook.com>
2024-11-16 14:04:52 -0300

    add graphic as a simple object

--- Resources/Pd/graphic.cpp
+++ Resources/Pd/graphic.cpp
@@ -35,6 +35,14 @@ void graphic_getrect(t_gobj *z, t_glist *glist, int *xp1, int *yp1, int *xp2, in
 }
 
 // ─────────────────────────────────────
+void graphic_delete(t_gobj *z, t_glist *glist) {
+    graphic *x = (graphic *)z;
+    t_canvas *canvas = glist_getcanvas(glist);
+    std::string tag = "graphic_" + std::to_string(reinterpret_cast<unsigned long>(x));
+    pdgui_vmess(0, "crr rs", canvas, "delete", tag.c_str());
+}
+
+// ─────────────────────────────────────
 void graphic_draw(graphic *x, t_glist *glist) {
     t_canvas *canvas = glist_getcanvas(glist);
     std::string tag = "graphic_" + std::to_string(reinterpret_cast<unsigned long>(x));
@@ -52,25 +60,29 @@ void graphic_displace(t_gobj *z, t_glist *glist, int dx, int dy) {
     graphic *x = (graphic *)z;
     x->x_obj.te_xpix += dx;
     x->x_obj.te_ypix += dy;
+    t_canvas *canvas = glist_getcanvas(glist);
+    graphic_delete(z, glist);
+    graphic_draw(x, glist);
+    canvas_fixlinesfor(canvas, (t_text *)x);
 }
+
 
 // ─────────────────────────────────────
 void graphic_select(t_gobj *z, t_glist *glist, int state) {
     graphic *x = (graphic *)z;
+    t_canvas *canvas = glist_getcanvas(glist);
+    int x1, y1, width, height;
+    graphic_getrect(z, glist, &x1, &y1, &width, &height);
+    std::string tag = "graphic_" + std::to_string(reinterpret_cast<unsigned long>(x));
     if (state) {
-        int x1, y1, width, height;
-        graphic_getrect(z, glist, &x1, &y1, &width, &height);
-        pdgui_vmess(0, "crr iiii rs",
-                        glist_getcanvas(glist),
-                        "create", "rectangle",
-                        x1 - 4, y1 - 4,
-                        x1 + width + 4,
-                        "-outline", "blue",
-                        "-tags", "select_rect");
+        post("selected");
+        
     } else {
-        pdgui_vmess(0, "crr rs",
-                          glist_getcanvas(glist),
-                          "delete", "select_rect");
+        pdgui_vmess(0, "crr iiii rs",
+                      canvas,
+                      "create", "rectangle",
+                      x1, y1, width + 180, height + 180,
+                      "-tags", tag.c_str());
     }
 }
 
@@ -78,9 +90,7 @@ void graphic_select(t_gobj *z, t_glist *glist, int state) {
 void graphic_activate(t_gobj *z, t_glist *glist, int state) {
 }
 
-// ─────────────────────────────────────
-void graphic_delete(t_gobj *z, t_glist *glist) {
-}
+
 
 // ─────────────────────────────────────
 void graphic_vis(t_gobj *z, t_glist *glist, int flag) {
@@ -101,23 +111,36 @@ int graphic_click(t_gobj *z, t_glist *glist, int xpix, int ypix, int shift, int 
 // ─────────────────────────────────────
 void *graphic_new(void) {
     graphic *x = (graphic *)pd_new(graphic_class);
-    x->x_clock = clock_new(x, (t_method)graphic_draw);
-    return (void *)x;
+    return x;
 }
 
 // ─────────────────────────────────────
-void graphic_proxynew(t_py4pd_edit_proxy *p) {
+void graphic_free(graphic *x) {
+    // delete the graphic
+    //t_canvas *canvas = glist_getcanvas(x->x_obj);
+    //std::string tag = "graphic_" + std::to_string(reinterpret_cast<unsigned long>(x));
+    //pdgui_vmess(0, "crr rs", canvas, "delete", tag.c_str());
+    //pd_free(&x->x_obj.ob_pd);
+
+}
+
+// ─────────────────────────────────────
+void graphic_proxyany(t_graphic_proxy *p, t_symbol *s, int ac, t_atom *av) {
+
+}
+
+// ─────────────────────────────────────
+void graphic_proxynew(t_graphic_proxy *p) {
     pd_unbind(&p->p_obj.ob_pd, p->p_sym);
-    clock_free(p->p_clock);
+    //clock_free(p->p_clock);
     pd_free(&p->p_obj.ob_pd);
 }
 
 // ─────────────────────────────────────
 t_graphic_proxy *graphic_proxyedit(graphic *x, t_symbol *s) {
     t_graphic_proxy *p = (t_graphic_proxy *)pd_new(graphic_proxy);
-    p->p_cnv = x;
+    //p->p_cnv = x;
     pd_bind(&p->p_obj.ob_pd, p->p_sym = s);
-    p->p_clock = clock_new(p, (t_method)Py4pdPic_EditProxyFree);
     return (p);
 }
 
@@ -133,13 +156,12 @@ extern "C" void graphic_setup(void) {
     graphic_widgetbehavior.w_visfn = graphic_vis;
 
     graphic_widgetbehavior.w_displacefn = graphic_displace;
-    //graphic_widgetbehavior.w_selectfn = graphic_select;
+    graphic_widgetbehavior.w_selectfn = graphic_select;
     //graphic_widgetbehavior.w_activatefn = graphic_activate;
     //graphic_widgetbehavior.w_deletefn = graphic_delete;
     //graphic_widgetbehavior.w_clickfn = graphic_click;
     class_setwidget(graphic_class, &graphic_widgetbehavior);
-    Py4pdEditProxy = class_new(0, 0, 0, sizeof(t_py4pd_edit_proxy), CLASS_NOINLET | CLASS_PD, 0);
-
-        class_addanything(Py4pdEditProxy, Py4pdPic_EditProxyAny);
+    graphic_proxy = class_new(0, 0, 0, sizeof(t_graphic_proxy), CLASS_NOINLET | CLASS_PD, A_NULL);
+    class_addanything(graphic_proxy, graphic_proxyany);
 
 }
