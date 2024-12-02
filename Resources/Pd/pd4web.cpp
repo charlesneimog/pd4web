a51a5f68f83fe3c13469619d90100ee6fc78736f
Charles K. Neimog <charlesneimog@outlook.com>
2024-12-02 13:56:02 -0300

    bump dev version

--- Resources/Pd/pd4web.cpp
+++ Resources/Pd/pd4web.cpp
@@ -20,7 +20,7 @@
 static bool global_pd4web_check = false;
 static t_class *pd4web_class;
 
-#define PD4WEB_EXTERNAL_VERSION "2.2.8"
+#define PD4WEB_EXTERNAL_VERSION "2.3.0"
 
 // ─────────────────────────────────────
 class Pd4Web {
